bbede2640b383b775f794c4038dd3678ae5f33bc
alexa <alexagastelum571@gmail.com>
2024-10-21 14:49:09 -0700

    Agregar archivos de proyecto.

---
+++ nombre MVVM_implementacion_AMGD/VistaModelo/VMpatron.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nombre_MVVM_implementacion_AMGD.VistaModelo
+{
+     class VMpatron : BaseViewModel
+    {
+        #region VARIABLES
+        string _Texto;
+        #endregion
+        #region CONSTRUCTOR
+
+        public string Texto
+        {
+            get { return _Texto; }
+            set { SetValue(ref _Texto, value); }
+        }
+        #endregion
+        #region PROCESOS
+
+        public async Task ProccesoAsyncrong() 
+        {
+        }
+        public void ProcesoSimple() 
+        {
+        }
+        #endregion
+
+    }
+}
