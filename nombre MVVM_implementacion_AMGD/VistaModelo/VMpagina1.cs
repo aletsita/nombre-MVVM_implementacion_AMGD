cb3902910815032565e7ea24ba2e5f9666a2e853
aletsita <alexagastelum577@gmail.com>
2024-11-07 10:47:53 -0700

    Practica 2.3

--- nombre MVVM_implementacion_AMGD/VistaModelo/VMpagina1.cs
+++ nombre MVVM_implementacion_AMGD/VistaModelo/VMpagina1.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
@@ -9,6 +7,13 @@ namespace nombre_MVVM_implementacion_AMGD.VistaModelo
 {
     internal class VMpagina1 : BaseViewModel
     {
+        #region VARIABLES
+        string _N1;
+        string _N2;
+        string _R;
+        string _Mensaje;
+        #endregion
+
         #region CONSTRUCTOR
         public VMpagina1(INavigation navigation)
         {
@@ -16,10 +21,51 @@ namespace nombre_MVVM_implementacion_AMGD.VistaModelo
         }
         #endregion
 
+        #region OBJETOS
+        public string N1 
+        {
+            get { return _N1; }
+            set { SetValue(ref _N1, value); }
+        }
+
+        public string N2
+        {
+            get { return _N2; }
+            set { SetValue(ref _N2, value); }
+        }
+
+        public string R
+        {
+            get { return _R; }  
+            set { SetValue(ref _R, value); }
+        }
+
+        public string Mensaje 
+        {
+            get { return _Mensaje; }
+            set { SetValue(ref _Mensaje, value); }
+        }
+        #endregion
+
         #region PROCESOS
+        public async Task Procesoasync() 
+        { }
+
+        public void Sumar() 
+        {
+            double n1 = 0;
+            double n2 = 0;
+            double r = 0;
+
+            n1 = Convert.ToDouble(N1);
+            n2 = Convert.ToDouble(N2);
+            r = n1 + n2;
+            R = r.ToString();
+        }
+
         public async Task Alerta()
         {
-            await DisplayAlert("Titulo", "Mensaje", "OK");
+            await DisplayAlert("Titulo", Mensaje, "OK");
         }
 
         public void ProcesoSimple()
@@ -28,8 +74,8 @@ namespace nombre_MVVM_implementacion_AMGD.VistaModelo
         #endregion
 
         #region COMANDOS
-        public ICommand Alertacommand => new Command(async () => await Alerta());
-        public ICommand ProcesoSimpcommand => new Command(ProcesoSimple);
+        public ICommand suymarcommand => new Command(Sumar);
+        public ICommand Procesoasynccomand => new Command(async () => await Procesoasync());
         #endregion
     }
 }
